436ce8f587bb73b73b4908de17b49c876d797502
Colin Sherratt <colin.sherratt@gmail.com>
2015-04-14 22:05:07 -0400

    Moved to new Arc based api

--- src/lib.rs
+++ src/lib.rs
@@ -1,50 +1,107 @@
 extern crate atom;
 
-use std::sync::atomic::{AtomicBool, Ordering};
+use std::sync::Arc;
+use std::sync::atomic::{AtomicIsize, Ordering};
 use std::thread;
 use atom::Atom;
 
+struct Inner {
+    state: AtomicIsize,
+    waiting: Atom<Waiting, Box<Waiting>>
+}
+
+#[derive(Copy, Clone, Debug, PartialEq, Eq)]
 enum State {
-    ThreadWaiting(thread::Thread)
+    Idle = 0is,
+    Triggered = 1is,
+    Dropped = 2is
 }
 
 impl State {
-    fn trigger(self) {
-        match self {
-            State::ThreadWaiting(thread) => thread.unpark()
-        }        
-    }
-}
-
-pub struct Pulse {
-    fired: AtomicBool,
-    state: Atom<State, Box<State>>
-}
-
-impl Pulse {
-    pub fn new() -> Pulse {
-        Pulse {
-            fired: AtomicBool::new(false),
-            state: Atom::empty()
+    fn from_isize(v: isize) -> State {
+        match v {
+            0 => State::Idle,
+            1 => State::Triggered,
+            2 => State::Dropped,
+            v => panic!("read invalid State {}", v)
         }
     }
+}
 
-    // Check if the signal has been triggered or not
-    pub fn triggered(&self) -> bool {
-        self.fired.load(Ordering::Relaxed)
+pub enum Waiting {
+    Thread(thread::Thread)
+}
+
+impl Waiting {
+    fn trigger(self) {
+        match self {
+            Waiting::Thread(thread) => thread.unpark()
+        }        
     }
 
-    /// Trigger an pulse, this can only occure once
-    /// Returns true if this triggering triggered the pulse
-    pub fn trigger(&self) -> bool {
-        let old = self.fired.swap(true, Ordering::Relaxed);
-        if old == false {
-            match self.state.take(Ordering::Acquire) {
+    pub fn thread() -> Waiting {
+        Waiting::Thread(thread::current())
+    }
+}
+
+pub struct Trigger(Arc<Inner>);
+
+impl Trigger {
+    fn set(&self, state: State) {
+        let old = self.0.state.compare_and_swap(
+            State::Idle as isize,
+            state as isize,
+            Ordering::Relaxed
+        );
+
+        if old == State::Idle as isize {
+            match self.0.waiting.take(Ordering::Acquire) {
                 None => (),
                 Some(v) => v.trigger()
             }
         }
-        old == false
+    }
+
+    /// Trigger an pulse, this can only occure once
+    /// Returns true if this triggering triggered the pulse
+    pub fn trigger(self) { self.set(State::Triggered) }
+}
+
+pub struct Pulse(Arc<Inner>);
+
+impl Pulse {
+    pub fn new() -> (Pulse, Trigger) {
+        let inner = Arc::new(Inner {
+            state: AtomicIsize::new(0),
+            waiting: Atom::empty()
+        });
+
+        (Pulse(inner.clone()), Trigger(inner.clone()))
+    }
+
+    // Check if the signal has been triggered or not
+    pub fn triggered(&self) -> bool {
+        match State::from_isize(self.0.state.load(Ordering::Relaxed)) {
+            State::Triggered | State::Dropped => true,
+            _ => false
+        }
+    }
+
+    /// Arm a pulse to wake 
+    pub fn arm(&self, waiter: Box<Waiting>) {
+        let old = self.0.waiting.swap(
+            waiter,
+            Ordering::AcqRel
+        );
+
+        if old.is_some() {
+            panic!("Pulse cannot be waited on by multiple clients");
+        }        
+    }
+
+    /// Disarm a pulse
+    pub fn disarm(&self) {
+        self.0.waiting.take(Ordering::Acquire);
     }
 
     /// Wait for an pulse to be triggered
@@ -56,18 +113,11 @@ impl Pulse {
                 return;
             }
 
-            let old = self.state.swap(
-                Box::new(State::ThreadWaiting(thread::current())),
-                Ordering::AcqRel
-            );
-
-            if old.is_some() {
-                panic!("Pulse cannot be waited on by multiple clients");
-            }
+            self.arm(Box::new(Waiting::thread()));
 
             if self.triggered() {
                 // cleanup the state. since we set it
-                self.state.take(Ordering::Acquire);
+                self.disarm();
             } else {
                 // wait for wake
                 thread::park();
--- tests/simple.rs
+++ tests/simple.rs
@@ -2,52 +2,34 @@
 extern crate pulse;
 
 use std::thread;
-use std::sync::Arc;
-use pulse::Pulse;
+use pulse::*;
 
 #[test]
 fn wait() {
-    let p = Pulse::new();
+    let (p, t) = Pulse::new();
     assert!(!p.triggered());
-    assert!(p.trigger());
+    t.trigger();
     assert!(p.triggered());
-    assert!(!p.trigger());
 }
 
 #[test]
 fn wake_post() {
-    let p = Pulse::new();
+    let (p, t) = Pulse::new();
     assert!(!p.triggered());
-    p.trigger();
+    t.trigger();
     assert!(p.triggered());
     p.wait();
 }
 
 #[test]
 fn wake_thread_spawn() {
-    let p = Arc::new(Pulse::new());
+    let (p, t) = Pulse::new();
     assert!(!p.triggered());
-    let pc = p.clone(); 
     thread::spawn(|| {
-        let pc = pc;
         thread::sleep_ms(10);
-        pc.trigger();
+        t.trigger();
     });
     assert!(!p.triggered());
     p.wait();
     assert!(p.triggered());
-}
-
-#[test]
-fn wake_thread_scope() {
-    let p = Pulse::new();
-    assert!(!p.triggered());
-    let guard = thread::scoped(|| {
-        thread::sleep_ms(10);
-        p.trigger();
-    });
-    assert!(!p.triggered());
-    p.wait();
-    assert!(p.triggered());
-    guard.join();
 }
